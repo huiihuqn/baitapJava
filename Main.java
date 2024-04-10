8732ed4b16b7c4b9f770f231037503a899003888
huiihuqn <153249593+huiihuqn@users.noreply.github.com>
2024-04-10 09:21:45 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
 package baitapGK;
+
 import java.io.*;
 import java.util.*;
 import javax.xml.parsers.*;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import org.w3c.dom.*;
 import java.security.*;
 import java.text.ParseException;
@@ -16,6 +22,7 @@ class Student {
     int age;
     int sum;
     boolean isDigit;
+
     public Student(int id, String name, String address, String dateOfBirth) {
         this.id = id;
         this.name = name;
@@ -35,7 +42,7 @@ class Thread1 implements Runnable {
         try {
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db = dbf.newDocumentBuilder();
-            Document doc = db.parse("student.xml");
+            Document doc = db.parse("./src/baitapGK/student.xml");
 
             NodeList studentList = doc.getElementsByTagName("student");
             for (int i = 0; i < studentList.getLength(); i++) {
@@ -47,7 +54,6 @@ class Thread1 implements Runnable {
                     String name = studentElement.getElementsByTagName("name").item(0).getTextContent();
                     String address = studentElement.getElementsByTagName("address").item(0).getTextContent();
                     String dateOfBirth = studentElement.getElementsByTagName("dateOfBirth").item(0).getTextContent();
-
                     students.add(new Student(id, name, address, dateOfBirth));
                 }
             }
@@ -68,11 +74,10 @@ class Thread2 implements Runnable {
         for (Student student : students) {
             Calendar dob = Calendar.getInstance();
             try {
-				dob.setTime(new SimpleDateFormat("yyyy-MM-dd").parse(student.dateOfBirth));
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+                dob.setTime(new SimpleDateFormat("yyyy-MM-dd").parse(student.dateOfBirth));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
             int age = Calendar.getInstance().get(Calendar.YEAR) - dob.get(Calendar.YEAR);
 
             MessageDigest md = null;
@@ -116,7 +121,11 @@ class Thread3 implements Runnable {
                     break;
                 }
             }
-            student.isDigit = isPrime;
+            if (isPrime) {
+                System.out.println("ID: " + student.id + " Name: " + student.name + " Address: " + student.address + " Age: " + student.age + " Sum of date of birth digits: " + student.sum + " is a prime number");
+            } else {
+                System.out.println("ID: " + student.id + " Name: " + student.name + " Address: " + student.address + " Age: " + student.age + " Sum of date of birth digits: " + student.sum + " is not a prime number");
+            }
         }
     }
 }
@@ -129,22 +138,58 @@ class Thread4 implements Runnable {
     }
 
     public void run() {
-        for (Student student : students) {
-            System.out.println("Student " + student.id + " is digit: " + (student.isDigit ? "Yes" : "No"));
-            System.out.println("Student " + student.id + " age: " + student.age);
-            System.out.println("Student " + student.id + " sum: " + student.sum);
-            System.out.println("Student " + student.id + " name: " + student.name);
-            System.out.println("Student " + student.id + " address: " + student.address);
-            System.out.println("Student " + student.id + " dateOfBirth: " + student.dateOfBirth);
-            System.out.println();
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.newDocument();
+
+            Element root = doc.createElement("students");
+            doc.appendChild(root);
+
+            for (Student student : students) {
+                Element studentElement = doc.createElement("student");
+                root.appendChild(studentElement);
+
+                Element idElement = doc.createElement("id");
+                idElement.setTextContent(Integer.toString(student.id));
+                studentElement.appendChild(idElement);
+
+                Element nameElement = doc.createElement("name");
+                nameElement.setTextContent(student.name);
+                studentElement.appendChild(nameElement);
+
+                Element addressElement = doc.createElement("address");
+                addressElement.setTextContent(student.address);
+                studentElement.appendChild(addressElement);
+
+                Element ageElement = doc.createElement("age");
+                ageElement.setTextContent(Integer.toString(student.age));
+                studentElement.appendChild(ageElement);
+
+                Element sumElement = doc.createElement("sum");
+                sumElement.setTextContent(Integer.toString(student.sum));
+                studentElement.appendChild(sumElement);
+
+                Element isDigitElement = doc.createElement("isDigit");
+                isDigitElement.setTextContent(Boolean.toString(student.isDigit));
+                studentElement.appendChild(isDigitElement);
+            }
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File("./src/baitapGK/kq.xml"));
+
+            transformer.transform(source, result);
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
-
 public class Main {
     public static void main(String[] args) {
         List<Student> students = new ArrayList<>();
-
         Thread t1 = new Thread(new Thread1(students));
         Thread t2 = new Thread(new Thread2(students));
         Thread t3 = new Thread(new Thread3(students));
@@ -158,14 +203,19 @@ public class Main {
         }
 
         t2.start();
-        t3.start();
-
         try {
             t2.join();
-            t3.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        t3.start();
+        try {
+			t3.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
         t4.start();
     }
